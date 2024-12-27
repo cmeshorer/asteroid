522cd5b7840199a54df8732415d4015ed0f71132
cmeshorer <cmeshorer@gmail.com>
2024-12-27 16:08:57 +0100

    chore: add font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,31 @@
+import {
+  Oxanium_400Regular,
+  Oxanium_700Bold,
+  useFonts,
+} from "@expo-google-fonts/oxanium";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 
 const RootLayout = () => {
+  const [loaded, error] = useFonts({
+    Oxanium_400Regular,
+    Oxanium_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <Stack>
