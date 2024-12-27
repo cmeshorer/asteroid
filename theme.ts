2d4ffc65f5e0dd23edec604f188efd5135306c0f
cmeshorer <cmeshorer@gmail.com>
2024-12-27 18:04:09 +0100

    chore: add theme

---
+++ theme.ts
@@ -0,0 +1,41 @@
+import { ColorValue } from "react-native";
+
+interface Theme {
+  primary: ColorValue;
+  secondary: ColorValue;
+  tertiary: ColorValue;
+  complementary: {
+    bazaar: ColorValue;
+    brick: ColorValue;
+    cinnabar: ColorValue;
+    pacific: ColorValue;
+    steel: ColorValue;
+    tangerine: ColorValue;
+  };
+  grey: {
+    black: ColorValue;
+    dark: ColorValue;
+    regular: ColorValue;
+    white: ColorValue;
+  };
+}
+
+export const theme = {
+  primary: "#3a506b",
+  secondary: "#1c2541",
+  tertiary: "#0b132b",
+  complementary: {
+    bazaar: "#987680",
+    brick: "#af1740",
+    cinnabar: "#eb4034",
+    pacific: "#219ebc",
+    steel: "#bd35bb",
+    tangerine: "#fc8756",
+  },
+  grey: {
+    black: "#000000",
+    dark: "#2e2e2e",
+    regular: "#8e9191",
+    white: "#ffffff",
+  },
+} as Theme;
