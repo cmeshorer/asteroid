b44bf3b890eaa7620229195c7def6b37dc944296
cmeshorer <cmeshorer@gmail.com>
2024-12-28 22:27:53 +0100

    chore: add constants

---
+++ constants.ts
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native";
+import { theme } from "./theme";
+
+export const sizes = {
+  elements: {
+    animation: 300,
+    icon: {
+      medium: 28,
+      large: 36,
+    },
+    input: 50,
+    text: {
+      medium: 16,
+      big: 18,
+      large: 22,
+      major: 26,
+      huge: 32,
+    },
+  },
+  radius: {
+    medium: 16,
+    large: 22,
+  },
+  spacing: {
+    tiny: 4,
+    small: 12,
+    medium: 16,
+    large: 32,
+    major: 48,
+  },
+};
+
+export const values = {
+  opacity: {
+    transparent: 0,
+    half: 0.5,
+    opaque: 1,
+  },
+};
+
+export const styles = StyleSheet.create({
+  shadows: {
+    shadowColor: theme.grey.dark,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.2,
+    elevation: 4,
+  },
+});
