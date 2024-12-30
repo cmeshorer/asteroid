fb6c46bc4a18ae32f28a3afd7e83ff11affbe83b
cmeshorer <cmeshorer@gmail.com>
2024-12-30 18:35:37 +0100

    feat: like animation

--- components/buttons/like/component.tsx
+++ components/buttons/like/component.tsx
@@ -1,26 +1,48 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 import { toggleAsteroidLike } from "@/asteroidsSlice";
+import { sizes } from "@/constants";
 import { useAppDispatch } from "@/hooks";
 import Entypo from "@expo/vector-icons/Entypo";
 import { theme } from "@/theme";
 import { LikeButtonProps } from "./types";
-import { sizes } from "@/constants";
 
 const LikeButton = ({ asteroidId, isLiked }: LikeButtonProps) => {
   const dispatch = useAppDispatch();
+  const scale = useSharedValue(1);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
   const onLike = () => {
+    scale.value = withSpring(
+      1.5,
+      {
+        damping: 3,
+        stiffness: 150,
+      },
+      () => {
+        scale.value = withSpring(1);
+      }
+    );
     dispatch(toggleAsteroidLike(asteroidId));
   };
 
   return (
-    <TouchableOpacity onPress={onLike}>
-      <Entypo
-        name={`heart${isLiked ? "" : "-outlined"}`}
-        color={isLiked ? theme.complementary.brick : theme.grey.dark}
-        size={sizes.elements.icon.medium}
-      />
-    </TouchableOpacity>
+    <Animated.View style={animatedStyle}>
+      <TouchableOpacity onPress={onLike}>
+        <Entypo
+          name={`heart${isLiked ? "" : "-outlined"}`}
+          color={isLiked ? theme.complementary.brick : theme.grey.dark}
+          size={sizes.elements.icon.medium}
+        />
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
