c573ab0f8a12fa019781e3a2d7098b3cedbe39c4
cmeshorer <cmeshorer@gmail.com>
2024-12-30 13:41:43 +0100

    feat: favorite asteroids

--- app/(tabs)/favorites/index.tsx
+++ app/(tabs)/favorites/index.tsx
@@ -1,5 +1,34 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { useAppSelector } from "@/hooks";
+import Page from "@/components/page";
+import AsteroidList from "@/components/asteroid/list";
+import Animation from "@/components/animation";
+
 const FavoritesScreen = () => {
-  return;
+  const styles = StyleSheet.create({
+    page: { paddingBottom: 0 },
+  });
+  const likedAsteroids = useAppSelector(
+    (state) => state.asteroids.likedAsteroids
+  );
+  const EmptyComponent = () => {
+    return (
+      <Animation
+        source={require("../../../assets/animations/space-book.json")}
+      />
+    );
+  };
+
+  return (
+    <Page style={styles.page}>
+      <AsteroidList
+        asteroids={likedAsteroids}
+        detailsPath="/favorites/details"
+        EmptyComponent={EmptyComponent}
+      />
+    </Page>
+  );
 };
 
 export default FavoritesScreen;
